e7a91bca6c116f4c45688ca54cb20ab1148f6a74
agrygo99 <agrygo@uiowa.edu>
2020-09-27 00:31:24 -0500

    added in buttons for newgame, reset, and quit

---
+++ Assets/Flappy Bird Style/Scripts/ButtonClicked.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonClicked : MonoBehaviour
+{
+
+    public void NewGame(){
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Reset(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+
+}
