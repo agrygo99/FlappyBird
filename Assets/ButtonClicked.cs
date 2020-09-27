87f74709b7389bf8e04e849b0a869af8fec57b8e
agrygo99 <agrygo@uiowa.edu>
2020-09-26 19:29:36 -0500

    added in high score, keeps resetting at new game. Added time elapsed

---
+++ Assets/ButtonClicked.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClicked : MonoBehaviour
+{
+    /*public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button newGame = button.GetComponent<Button>;
+        newGame.onClick.AddListener(newClick);
+    }
+
+    void newClick(){
+
+    }*/
+}
